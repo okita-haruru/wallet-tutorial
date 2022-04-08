d852132993b6fcd77581ab1f29226f602c2d9dab
okita-haruru <okitaharuru@gmail.com>
2022-04-08 02:02:58 -0700

    finished

--- pages/recover.tsx
+++ pages/recover.tsx
@@ -5,6 +5,8 @@ import { Form, Input, Button } from "antd";
 import { useGlobalState } from "../context";
 import { LoadingOutlined } from "@ant-design/icons";
 import styled from "styled-components";
+import * as Bip39 from "bip39"
+import { Keypair } from "@solana/web3.js";
 
 // Import Bip39 to convert a phrase to a seed:
 
@@ -28,13 +30,14 @@ const Recover: NextPage = () => {
     // (a) review the import guidance on lines 9 and 11
     // (b) convert the mnemonic to seed bytes
     // Documentation Reference: https://github.com/bitcoinjs/bip39
-    const seed = new Uint8Array();
+    const seed = (await Bip39.mnemonicToSeed(inputMnemonic)).slice(0,32);
+;
 
     // (c) use the seed to import the account (i.e. keypair)
     // Documentation Reference:
     //   https://solana-labs.github.io/solana-web3.js/classes/Keypair.html
     //   https://solana-labs.github.io/solana-web3.js/classes/Keypair.html#fromSeed
-    const importedAccount = null;
+    const importedAccount = Keypair.fromSeed(seed);
     setAccount(importedAccount);
 
     // (d) You can now delete the console.log statement since the function is implemented!
--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,5 @@
 // Import any additional classes and/or functions needed from Solana's web3.js library as you go along:
-import { Cluster, Keypair } from "@solana/web3.js";
+import { Cluster, clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { message } from "antd";
 
 // *Step 3*: implement a function that gets an account's balance
@@ -14,17 +14,17 @@ const refreshBalance = async (network: Cluster | undefined, account: Keypair | n
     //   https://solana-labs.github.io/solana-web3.js/classes/Connection.html
     //   https://solana-labs.github.io/solana-web3.js/modules.html#clusterApiUrl
     console.log("Balance functionality not implemented yet!");
-    const connection = "";
+    const connection = new Connection(clusterApiUrl(network),"confirmed");
 
     // (c) get the key using one of the accessors on the account passed in as an argument
     // Documentation Reference: https://solana-labs.github.io/solana-web3.js/classes/Keypair.html
-    const publicKey = "";
+    const publicKey = account.publicKey;
 
     // (d) get the account's balance using the connection instance
     // Documentation Reference: https://solana-labs.github.io/solana-web3.js/classes/Connection.html
-    const balance = 0;
+    const balance = connection.getBalance(publicKey);
 
-    return balance;
+    return await balance/LAMPORTS_PER_SOL;
     // (e) You can now delete the console.log statement since the function is implemented!
   } catch (error) {
     const errorMessage =
@@ -46,20 +46,20 @@ const handleAirdrop = async (network: Cluster, account: Keypair | null) => {
     //   https://solana-labs.github.io/solana-web3.js/classes/Connection.html
     //   https://solana-labs.github.io/solana-web3.js/modules.html#clusterApiUrl
     console.log("Airdrop functionality not implemented yet!");
-    const connection = "";
+    const connection = new Connection(clusterApiUrl(network),"confirmed");
 
     // (c) get the key using one of the accessors on the account passed in as an argument
     // Documentation Reference: https://solana-labs.github.io/solana-web3.js/classes/Keypair.html
-    const publicKey = "";
+    const publicKey = account.publicKey;
 
     // (d) request the airdrop using the connection instance
     // Note that you should include the amount to airdrop (consider using the LAMPORTS_PER_SOL constant from the web3.js library)
     // Documentation Reference: https://solana-labs.github.io/solana-web3.js/classes/Connection.html
-    const confirmation = "";
+    const confirmation = await connection.requestAirdrop(publicKey,LAMPORTS_PER_SOL);
 
     // (d) confirm the transaction using the connection instance and the confirmation string returned from the airdrop
     // Documentation Reference: https://solana-labs.github.io/solana-web3.js/classes/Connection.html
-    const result = "";
+    const result = await connection.confirmTransaction(confirmation,"confirmed");
 
     // (e) Refactor the refreshBalance function to return balances in SOL instead of Lamports (Hint: LAMPORTS_PER_SOL)
 
